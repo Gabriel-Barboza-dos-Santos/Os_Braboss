238a41c9083c2fbca3c5514214f269faeb56bfa3
matfeerreira <143464374+matfeerreira@users.noreply.github.com>
2023-10-05 20:23:41 -0300

    Update matheusferreira.py

--- matheusferreira.py
+++ matheusferreira.py
@@ -1,1 +1,42 @@
+def soma(x, y):
+    return x + y
 
+def subtracao(x, y):
+    return x - y
+
+def multiplicacao(x, y):
+    return x * y
+
+def divisao(x, y):
+    if y == 0:
+        return "Não é possível dividir por zero."
+    else:
+        return x / y
+
+while True:
+    print("Selecione a operação:")
+    print("1. Soma")
+    print("2. Subtração")
+    print("3. Multiplicação")
+    print("4. Divisão")
+    print("5. Sair")
+
+    escolha = input("Digite a opção (1/2/3/4/5): ")
+
+    if escolha == '5':
+        print("Encerrando a calculadora.")
+        break
+
+    num1 = float(input("Digite o primeiro número: "))
+    num2 = float(input("Digite o segundo número: "))
+
+    if escolha == '1':
+        print("Resultado: ", soma(num1, num2))
+    elif escolha == '2':
+        print("Resultado: ", subtracao(num1, num2))
+    elif escolha == '3':
+        print("Resultado: ", multiplicacao(num1, num2))
+    elif escolha == '4':
+        print("Resultado: ", divisao(num1, num2))
+    else:
+        print("Opção inválida. Por favor, escolha uma opção válida.")
