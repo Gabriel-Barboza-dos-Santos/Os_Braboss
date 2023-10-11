2dc04773bb8d53bd5c85df23c86543ce0c462389
Vandraum <99769306+Vandraum@users.noreply.github.com>
2023-10-10 23:18:00 -0300

    Update gerador-de-senhas.py
    adição de código sem a necessidade de inserir valores

--- python/gerador-de-senhas.py
+++ python/gerador-de-senhas.py
@@ -1,4 +1,12 @@
 import random
 import string
 
+def gerar_senha(comprimento):
+    caracteres = string.ascii_letters + string.digits + string.punctuation
+    senha = ''.join(random.choice(caracteres) for _ in range(comprimento))
+    return senha
+
+comprimento_senha = 15
+senha_gerada = gerar_senha(comprimento_senha)
+print("Sua senha gerada:", senha_gerada)
 
