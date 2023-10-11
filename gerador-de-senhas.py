0f8bafb42776c9f64a4dabc0cfbc4fc90ebcf1a1
Vandraum <99769306+Vandraum@users.noreply.github.com>
2023-10-10 23:13:50 -0300

    Update gerador-de-senhas.py
    Mudança de codigo

--- gerador-de-senhas.py
+++ gerador-de-senhas.py
@@ -1,11 +1,2 @@
-from funcoes import *
-from interface import *
-
-cabecalho("GERADOR DE SENHAS")
-
-quant = quant_senhas()
-tam = tam_senhas()
-
-senhas = gerador_senhas(quant, tam)
-
-cabecalho("SAINDO...")
+import random
+import string
