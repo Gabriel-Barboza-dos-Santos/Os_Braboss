627101898ceb04b1863eef5b72a4e75469aa6e8a
Vandraum <99769306+Vandraum@users.noreply.github.com>
2023-10-10 23:14:43 -0300

    Update gerador-de-senhas.py
    campo pra gerar as senhas

--- gerador-de-senhas.py
+++ gerador-de-senhas.py
@@ -1,2 +1,8 @@
 import random
 import string
+
+def gerar_senha(comprimento):
+    caracteres = string.ascii_letters + string.digits + string.punctuation
+    senha = ''.join(random.choice(caracteres) for _ in range(comprimento))
+    return senha
+
